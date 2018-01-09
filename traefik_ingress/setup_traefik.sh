9826b47819c4394d735097ded4382153bbcf53fb
Mark Johnson <mfjohnson98@yahoo.com>
2018-01-08 22:22:07 -0500

    Initial COmmit

--- traefik_ingress/setup_traefik.sh
+++ traefik_ingress/setup_traefik.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+kubectl delete service --all
+kubectl delete deployments --all
+kubectl delete ingress --all
 kubectl apply -f nginxservice.yaml
-kubectl get service
-kubectl get pods
-kubectl get deployments
 kubectl apply -f traefix_service.yaml
+echo "------List services"
+kubectl get service
+echo "------List Pods"
+kubectl get pods
+echo "------List Deployments"
+kubectl get deployments
+echo "------List Ingress"
+kubectl get ingress
+echo "------List Taints"
+kubectl get nodes -o go-template-file="./nodes-taints.tmpl"
