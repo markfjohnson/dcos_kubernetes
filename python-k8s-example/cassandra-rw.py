c8b8342df01e815a9e426f72b5172cbda7175b73
markjohnson <markfjohnson@gmail.com>
2018-06-19 20:53:29 -0400

    Cassandra Sample program is now working.

--- python-k8s-example/cassandra-rw.py
+++ python-k8s-example/cassandra-rw.py
@@ -2,8 +2,30 @@ from cassandra.cluster import Cluster
 cluster = Cluster(["node-0-server.cassandra.autoip.dcos.thisdcos.directory",
                    "node-1-server.cassandra.autoip.dcos.thisdcos.directory",
                    "node-2-server.cassandra.autoip.dcos.thisdcos.directory"])
-session = cluster.connect("mykeyspace")
+session = cluster.connect()
+KEYSPACE = "dcos"
+rows = session.execute("SELECT keyspace_name FROM system_schema.keyspaces")
+if KEYSPACE in [row[0] for row in rows]:
+    print("dropping existing keyspace...")
+    session.execute("DROP KEYSPACE " + KEYSPACE)
 
+print("creating keyspace...")
+session.execute("""
+        CREATE KEYSPACE %s
+        WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+        """ % KEYSPACE)
+
+print("setting keyspace...")
+session.set_keyspace(KEYSPACE)
+print("creating table...")
+session.execute("""
+        CREATE TABLE users (
+            name text,
+            credits int,
+            user_id text,
+            PRIMARY KEY (name)
+        )
+        """)
 print("Getting ready to insert rows into the Cassandra table:")
 session.execute(
     """
@@ -18,6 +40,6 @@ print("Cassandra row inserts into USERS table is now complete.")
 print(" ")
 print("Users Table Contents")
 print("Name, Age, ID")
-rows = session.execute('SELECT name, age, email FROM users')
-for (name, age, email) in rows:
-    print name, age, email
+rows = session.execute('SELECT name, credits, user_id FROM users')
+for (name, credits, user_id) in rows:
+    print(name, credits, user_id)
